089645173c5c1f6e54ebbd7bd3d57b6bca1d692d
INS4NE4 <khmiadashvili21@gmail.com>
2019-12-13 10:57:14 -0800

    TravelWebPage

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+// NAVIGATION MENU
+const navbarToggler = document.querySelector(".toggler");
+const navbarLinks = document.getElementById('menu');
+navbarToggler.addEventListener('click', navbarTogglerClick);
+
+function navbarTogglerClick () {
+    navbarToggler.classList.toggle("open-toggler");
+    navbarLinks.classList.toggle("open");
+}
+// Login Open Form
+const loginToggler = document.getElementById('login_toggler');
+const loginContainer = document.getElementById('login_container');
+loginToggler.addEventListener('click', loginTogglerClick);
+
+function loginTogglerClick () {
+    loginContainer.classList.toggle("login_container");
+}
+
+// Login Form Close
+const loginTogglerClose = document.getElementById('login_close_toggler');
+const loginContainerClose = document.getElementById('login_container');
+loginTogglerClose.addEventListener('click', loginTogglerCloseClick);
+
+function loginTogglerCloseClick () {
+    loginContainerClose.classList.toggle("login_container");
+}
