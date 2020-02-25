4676505dfcb8cf9044654ab15795e5468bbd65d9
INS4NE4 <khmiadashvili21@gmail.com>
2020-02-25 01:14:26 -0800

    new

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,7 +15,6 @@ loginToggler.addEventListener('click', loginTogglerClick);
 function loginTogglerClick () {
     loginContainer.classList.toggle("login_container");
 }
-
 // Login Form Close
 const loginTogglerClose = document.getElementById('login_close_toggler');
 const loginContainerClose = document.getElementById('login_container');
@@ -24,3 +23,19 @@ loginTogglerClose.addEventListener('click', loginTogglerCloseClick);
 function loginTogglerCloseClick () {
     loginContainerClose.classList.toggle("login_container");
 }
+// Slider
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
